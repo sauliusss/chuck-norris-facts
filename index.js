18bc7a3313aec8672a2b8a8ae36481de7ae2173d
saulius <sjsaulius@gmail.com>
2023-08-01 16:51:25 +0300

    add api to project ( async and await)

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+const url = "https://api.chucknorris.io/jokes/random";
+getUrl();
+async function getUrl() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.value);
+  const fact = document.querySelector("#fact");
+  const newFact = document.createElement("div");
+  newFact.innerText = data.value;
+  fact.append(newFact);
+}
